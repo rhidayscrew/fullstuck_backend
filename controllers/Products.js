d75e3d43e771f9858dc726203065f8285beaf5b3
rhidayscrew17 <rhidayscrew@gmail.com>
2023-08-20 22:24:39 -0400

    revisi validasi prodUpdate prodDelete

--- controllers/Products.js
+++ controllers/Products.js
@@ -137,54 +137,101 @@ export const saveProduct = async (req, res) => {
 };
 
 export const updateProduct = async (req, res) => {
-  const errors = validationResult(req);
-  const product = await Product.findOne({
-    where: {
-      id: req.params.id,
-    },
-  });
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      success: false,
-      message: "Validation failure!",
-      errors: errors.errors,
-    });
-  }
-
-  if (!product) return res.status(404).json({ msg: "No Data Found" });
-
-  let fileName = "";
-  if (req.files === null) {
-    fileName = product.image;
-  } else {
-    const file = req.files.file;
-    const fileSize = file.data.length;
-    const ext = path.extname(file.name);
-    fileName = file.md5 + ext;
-    const allowedType = [".png", ".jpg"];
-
-    if (!allowedType.includes(ext.toLowerCase()))
-      return res.status(422).json({ msg: "Invalid Images" });
-    if (fileSize > 100000)
-      return res
-        .status(422)
-        .json({ msg: "Gambar harus lebih kecil dari 100 KB" });
-
-    const filepath = `./public/images/${product.image}`;
-    fs.unlinkSync(filepath);
-
-    file.mv(`./public/images/${fileName}`, (err) => {
-      if (err) return res.status(500).json({ msg: err.message });
-    });
-  }
-  const name = req.body.title;
-  const harbel = req.body.harbel;
-  const harjul = req.body.harjul;
-  const stok = req.body.stok;
-  const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
-
   try {
+    const product = await Product.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!product)
+      return res.status(404).json({
+        code: "404",
+        status: "Not Found",
+        message: "Data tidak di temukan",
+      });
+
+    let fileName = "";
+    if (req.files === null) {
+      fileName = product.image;
+    } else {
+      const file = req.files.file;
+      const fileSize = file.data.length;
+      const ext = path.extname(file.name);
+      fileName = file.md5 + ext;
+      const allowedType = [".png", ".jpg", "jpeg"];
+
+      if (!allowedType.includes(ext.toLowerCase()))
+        return res.status(422).json({
+          code: "422",
+          status: "Not Found",
+          message: "Invalid Images extension",
+        });
+      if (fileSize > 100000)
+        return res.status(422).json({
+          code: "422",
+          status: "Not Found",
+          message: "Image must be less than 5 MB",
+        });
+
+      const filepath = `./public/images/${product.image}`;
+      fs.unlinkSync(filepath);
+
+      file.mv(`./public/images/${fileName}`, (err) => {
+        if (err) return res.status(500).json({ msg: err.message });
+      });
+    }
+    const name = req.body.title;
+    const harbel = req.body.harbel;
+    const harjul = req.body.harjul;
+    const stok = req.body.stok;
+    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+
+    // Validasi field name, stock, harga_beli, dan harga_jual
+    const message = {};
+
+    if (!name) {
+      return res.status(422).json({
+        code: "422",
+        status: "Bad Request",
+        message: "Nama produk harus diisi",
+      });
+    } else if (name.length < 3) {
+      return res.status(422).json({
+        code: "422",
+        status: "Bad Request",
+        message: "Nama produk terlalu pendek",
+      });
+    }
+    if (!harbel || harbel <= 0)
+      return res.status(422).json({
+        code: "422",
+        status: "Bad Request",
+        message: "Harga beli produk harus diisi dengan angka lebih dari 0",
+      });
+
+    if (!harjul || harjul <= 0) {
+      return res.status(422).json({
+        code: "422",
+        status: "Bad Request",
+        message: "Harga jual produk harus diisi dengan angka lebih dari 0",
+      });
+    } else if (harjul <= harbel) {
+      return res.status(422).json({
+        code: "422",
+        status: "Bad Request",
+        message: "Harga jual produk harus lebih tinggi daripada harga beli",
+      });
+    }
+
+    if (!stok || stok <= 0) {
+      return res.status(422).json({
+        code: "422",
+        status: "Bad Request",
+        message: "Stok produk harus diisi dengan angka lebih dari 0",
+      });
+    }
+
     await Product.update(
       {
         name: name,
@@ -200,21 +247,35 @@ export const updateProduct = async (req, res) => {
         },
       }
     );
-    res.status(200).json({ msg: "Barang Updated Successfuly" });
+    res.status(200).json({
+      code: "200",
+      status: "Ok",
+      messege: "Product Updated Successfuly",
+    });
   } catch (error) {
-    console.log(error.message);
+    // console.log(error.message);
+    res.status(500).json({
+      code: "500",
+      status: "Internal Server Erro",
+      message: "Terjadi kesalahan pada server",
+    });
   }
 };
 
 export const deleteProduct = async (req, res) => {
-  const product = await Product.findOne({
-    where: {
-      id: req.params.id,
-    },
-  });
-  if (!product) return res.status(404).json({ msg: "No Data Found" });
-
   try {
+    const product = await Product.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!product)
+      return res.status(404).json({
+        code: "404",
+        status: "Not Found",
+        message: "No Data Found",
+      });
+
     const filepath = `./public/images/${product.image}`;
     fs.unlinkSync(filepath);
     await Product.destroy({
@@ -222,8 +283,17 @@ export const deleteProduct = async (req, res) => {
         id: req.params.id,
       },
     });
-    res.status(200).json({ msg: "Barang Deleted Successfuly" });
+    res.status(200).json({
+      code: "200",
+      status: "Ok",
+      messege: "Product Deleted Successfuly",
+    });
   } catch (error) {
-    console.log(error.message);
+    // console.log(error.message);
+    res.status(500).json({
+      code: "500",
+      status: "Internal Server Erro",
+      message: "Terjadi kesalahan pada server",
+    });
   }
 };
