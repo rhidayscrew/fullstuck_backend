80259c0d0371177a50548f68d2184e766f9dc038
rhidayscrew17 <rhidayscrew@gmail.com>
2023-08-26 23:35:36 -0400

    update modul userps

--- controllers/Userps.js
+++ controllers/Userps.js
@@ -1,40 +1,52 @@
 import User from "../models/UserPsModel.js";
-import {Op} from "sequelize";
+import { Op } from "sequelize";
 
-export const getUsersps = async(req, res) =>{
-    const page = parseInt(req.query.page) || 0;
-    const limit = parseInt(req.query.limit) || 10;
-    const search = req.query.search_query || "";
-    const offset = limit * page;
-    const totalRows = await User.count({
-        where:{
-            [Op.or]: [{name:{
-                [Op.like]: '%'+search+'%'
-            }}, {email:{
-                [Op.like]: '%'+search+'%'
-            }}]
-        }
-    });
-    const totalPage = Math.ceil(totalRows / limit);
-    const result = await User.findAll({
-        where:{
-            [Op.or]: [{name:{
-                [Op.like]: '%'+search+'%'
-            }}, {email:{
-                [Op.like]: '%'+search+'%'
-            }}]
+export const getUsersps = async (req, res) => {
+  const page = parseInt(req.query.page) || 0;
+  const limit = parseInt(req.query.limit) || 10;
+  const search = req.query.search_query || "";
+  const offset = limit * page;
+  const totalRows = await User.count({
+    where: {
+      [Op.or]: [
+        {
+          name: {
+            [Op.like]: "%" + search + "%",
+          },
         },
-        offset: offset,
-        limit: limit,
-        order:[
-            ['id', 'DESC']
-        ]
-    });
-    res.json({
-        result: result,
-        page: page,
-        limit: limit,
-        totalRows: totalRows,
-        totalPage: totalPage
-    });
-}
+        {
+          email: {
+            [Op.like]: "%" + search + "%",
+          },
+        },
+      ],
+    },
+  });
+  const totalPage = Math.ceil(totalRows / limit);
+  const result = await User.findAll({
+    where: {
+      [Op.or]: [
+        {
+          name: {
+            [Op.like]: "%" + search + "%",
+          },
+        },
+        {
+          email: {
+            [Op.like]: "%" + search + "%",
+          },
+        },
+      ],
+    },
+    offset: offset,
+    limit: limit,
+    order: [["id", "DESC"]],
+  });
+  res.json({
+    result: result,
+    page: page,
+    limit: limit,
+    totalRows: totalRows,
+    totalPage: totalPage,
+  });
+};
